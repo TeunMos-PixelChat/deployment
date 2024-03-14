4a8dc147bfc849ec882430c738cf337e750f0aa8
teunmos <teun.mos@gmail.com>
2024-03-14 14:51:32 +0100

    front-end update

--- kubernetes_build_containers.sh
+++ kubernetes_build_containers.sh
@@ -3,9 +3,14 @@
 eval $(minikube docker-env)
 
 # Use the environment variables set by `eval` in subsequent commands
-docker build --build-arg REACT_APP__MESSAGE_API_URL="api" -t pixelchat-frontend:latest -f ./frontend/dockerfile ./frontend
+docker build --build-arg REACT_APP_API_URL="api" -t pixelchat-frontend:latest -f ./frontend/dockerfile ./frontend
 docker build -t pixelchat-message-api:latest -f ./message-api/dockerfile ./message-api
 docker build -t pixelchat-gateway:latest -f ./gateway/dockerfile ./gateway
 
 # Reset the environment variables
 eval $(minikube docker-env -u)
+
+
+# TODO: eval command is not always working
+# A way to do this is to build the images to docker-hub instead of locally
+# which is how it's supposed to work
