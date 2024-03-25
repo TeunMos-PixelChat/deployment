e05589bc84e4e75d89b5813018893d0347def7cf
teunmos <teun.mos@gmail.com>
2024-03-25 11:58:37 +0100

    updated commit hashes and scripts

--- kubernetes_build_containers.sh
+++ kubernetes_build_containers.sh
@@ -3,9 +3,9 @@
 eval $(minikube docker-env)
 
 # Use the environment variables set by `eval` in subsequent commands
-docker build --build-arg REACT_APP_API_URL="api" -t pixelchat-frontend:latest -f ./frontend/dockerfile ./frontend
-docker build -t pixelchat-message-api:latest -f ./message-api/dockerfile ./message-api
-docker build -t pixelchat-gateway:latest -f ./gateway/dockerfile ./gateway
+docker build --build-arg REACT_APP_API_URL="api" -t pixelchat-frontend:latest -f ./frontend/Dockerfile ./frontend
+docker build -t pixelchat-message-api:latest -f ./message-api/Dockerfile ./message-api
+docker build -t pixelchat-gateway:latest -f ./gateway/Dockerfile ./gateway
 
 # Reset the environment variables
 eval $(minikube docker-env -u)
